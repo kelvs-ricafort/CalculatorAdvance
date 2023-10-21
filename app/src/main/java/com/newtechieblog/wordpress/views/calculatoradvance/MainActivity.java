9d4154ee7154e7e6b848bb6571eec33d41f126c1
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-21 14:57:34 +0800

    Printing the numbers to the screen

--- app/src/main/java/com/newtechieblog/wordpress/views/calculatoradvance/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/calculatoradvance/MainActivity.java
@@ -3,12 +3,125 @@ package com.newtechieblog.wordpress.views.calculatoradvance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnZero, btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine,
+            btnAC, btnDel, btnAdd, btnSubtract, btnMultiply, btnDivide, btnEquals, btnDot;
+    private TextView textViewResult, textViewHistory;
+
+    private String number = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnZero = findViewById(R.id.btnZero);
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEight = findViewById(R.id.btnEight);
+        btnNine = findViewById(R.id.btnNine);
+
+        btnAdd = findViewById(R.id.btnPlus);
+        btnSubtract = findViewById(R.id.btnMinus);
+        btnMultiply = findViewById(R.id.btnMulti);
+        btnDivide = findViewById(R.id.btnDivide);
+
+        btnAC = findViewById(R.id.btnAC);
+        btnDel = findViewById(R.id.btnDel);
+        btnEquals = findViewById(R.id.btnEquals);
+        btnDot = findViewById(R.id.btnDot);
+
+        textViewResult = findViewById(R.id.textViewResult);
+        textViewHistory = findViewById(R.id.textViewHistory);
+
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("0");
+            }
+        });
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("1");
+            }
+        });
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("2");
+            }
+        });
+
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("3");
+            }
+        });
+
+        btnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("4");
+            }
+        });
+
+        btnFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("5");
+            }
+        });
+
+        btnSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("6");
+            }
+        });
+
+        btnSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("7");
+            }
+        });
+
+        btnEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("8");
+            }
+        });
+
+        btnNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberClick("9");
+            }
+        });
+    }
+
+    public void numberClick(String view) {
+        if (number == null) {
+            number = view;
+        } else {
+            number = number + view;
+        }
+
+        textViewResult.setText(number);
     }
 }
